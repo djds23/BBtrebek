6a6488fbbd0186bd1e80de4451de8f531531a9ad
Dean Silfen <dean.silfen@gmail.com>
2015-12-03 00:51:28 -0500

    Update Clue obj to strip html

--- BBtrebek/Clue.swift
+++ BBtrebek/Clue.swift
@@ -15,11 +15,24 @@ class Clue {
     var airdate: String
     var category: String
     
+    func stripHTMLTags(str: String) -> String {
+        return str.stringByReplacingOccurrencesOfString( "<[^>]+>",
+            withString: "",
+            options: .RegularExpressionSearch,
+            range: nil
+        )
+    }
+
     init(answer: String, question: String, value: Int, category: String, airdate: String) {
         self.answer = answer
         self.question = question
         self.value = value
         self.airdate = airdate
         self.category = category
+        
+        // this feels wrong, but I get compiler errors if I assign & call stripHTMLTags at the same time
+        self.answer = self.stripHTMLTags(self.answer)
+        self.question = self.stripHTMLTags(self.question)
+        self.category = self.stripHTMLTags(self.category)
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,8 @@ Swipe for Jeopardy Questions!
 
 <img width="432" alt="screen shot 2015-12-01 at 9 12 09 pm" src="https://cloud.githubusercontent.com/assets/5833968/11520328/7aafd2b8-9870-11e5-9cfd-df6c3b2345bc.png">
 
+TODO:
+  * either fix the randomizer in the rails app, or shuffle questions in app, you should only get 1 q per category every 50 questions
+  * move view to scroll view
+  * have the concept of players & a game, maybe swipe to different parts of the screen to add to different players scores
+
