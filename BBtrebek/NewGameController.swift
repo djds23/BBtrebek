89bf0e10b32ae3d37fa784d8cfbf3027fbcb61b2
Dean Silfen <dean.silfen@gmail.com>
2015-12-05 01:46:48 -0500

    Shell of next ViewController

---
+++ BBtrebek/NewGameController.swift
@@ -0,0 +1,13 @@
+//
+//  NewGameController.swift
+//  BBtrebek
+//
+//  Created by Dean Silfen on 12/5/15.
+//  Copyright (c) 2015 Dean Silfen. All rights reserved.
+//
+
+import UIKit
+
+class NewGameController: UIViewController {
+
+}
