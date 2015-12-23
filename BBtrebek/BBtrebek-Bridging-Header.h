ec4b82eac873bbd2de72ed812b12f6d89a9873a3
Dean Silfen <dean.silfen@gmail.com>
2015-12-22 23:15:02 -0500

    Add pods & bridge header to project

---
+++ BBtrebek/BBtrebek-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  BBtrebek-Bridging-Header.h
+//  BBtrebek
+//
+//  Created by Dean Silfen on 12/22/15.
+//  Copyright © 2015 Dean Silfen. All rights reserved.
+//
+
+#import <PureLayout/PureLayout.h>
+
+//#ifndef BBtrebek_Bridging_Header_h
+//#define BBtrebek_Bridging_Header_h
+
+
+
+//#endif /* BBtrebek_Bridging_Header_h */
---
+++ Pods/Target Support Files/Pods-BBtrebek/Pods-BBtrebek-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_BBtrebekVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BBtrebekVersionString[];
+
