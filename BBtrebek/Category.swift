1c7dcd2855d969d9f504285ab4b921e08e22e811
Dean Silfen <dean.silfen@gmail.com>
2016-11-26 20:53:04 -0500

    Add card count to category

--- BBtrebek/Category.swift
+++ BBtrebek/Category.swift
@@ -13,22 +13,33 @@ public class Category: NSObject {
     let title: String
     let id: Int
     var clues: Array<Clue> = []
+    var cardCount: Int?
 
     public func isRandom() -> Bool {
         return self.id == -1 ? true : false
     }
     
-    public init(title: String, id: Int) {
+    public func count() -> Int {
+        var output = self.clues.count
+        if self.clues.isEmpty && self.cardCount != nil {
+            output = self.cardCount!
+        }
+        return output
+    }
+    
+    
+    public init(title: String, id: Int, cardCount: Int? = nil) {
         self.title = title.stripHTMLTags()
         self.id = id
+        self.cardCount = cardCount
     }
 
     static public func initWithNSDictionary(_ dict: NSDictionary) -> Category? {
         var category = nil as Category?
-        if let title = dict["title"] as? String {
-            if let id = dict["id"] as? Int {
-                category = Category(title: title, id: id)
-            }
+        if let title = dict["title"] as? String,
+            let id = dict["id"] as? Int,
+            let cardCount = dict["card_count"] as? Int{
+            category = Category(title: title, id: id, cardCount: cardCount)
         }
         return category
     }
